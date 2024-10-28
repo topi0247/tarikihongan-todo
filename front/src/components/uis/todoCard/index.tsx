ecf8c21239f8b2182520987c75b0019d01ce125c
topi0247 <karubona.ra247@gmail.com>
2024-10-28 14:11:28 +0900

    MyPage作成

--- front/src/components/uis/todoCard/index.tsx
+++ front/src/components/uis/todoCard/index.tsx
@@ -1,16 +1,27 @@
 import { Todo } from "types";
 
 export default function TodoCard({ todo }: { todo: Todo }) {
+  const currentUser = { id: 1, name: "User 1" };
+  const isCreateUserDone = todo.doneUsers.some(
+    (todoUser) => todoUser.id === currentUser.id
+  );
+
   return (
     <div className="card bg-base-100 w-full shadow-xl border">
       <div className="card-body">
         <p>{todo.title}</p>
-        <p className="text-end text-sm">
-          created by {todo.user.name} ... created{" "}
-          <time dateTime={todo.created_at}>{todo.created_at}</time>
-        </p>
+        {currentUser.id !== todo.user.id && (
+          <p className="text-end text-sm">
+            created by {todo.user.name} ... created{" "}
+            <time dateTime={todo.created_at}>{todo.created_at}</time>
+          </p>
+        )}
         <div className="card-actions justify-start">
-          <button className="btn btn-primary px-3 py-2">やったよ！</button>
+          {isCreateUserDone ? (
+            <p className="btn btn-disabled">終わったよ！</p>
+          ) : (
+            <button className="btn btn-primary px-3 py-2">やったよ！</button>
+          )}
           <div className="collapse bg-base-200">
             <input type="checkbox" />
             <div className="collapse-title flex justify-between items-center px-4">
--- front/src/features/mypage/index.tsx
+++ front/src/features/mypage/index.tsx
@@ -1,3 +1,65 @@
+import TodoCard from "components/uis/todoCard";
+
+const Todos = Array.from({ length: 10 }, (_, i) => ({
+  id: i,
+  title: `Todo ${i}`,
+  user: { id: i, name: `User ${i}` },
+  created_at: "2021/09/01",
+  doneUsers: Array.from({ length: 3 }, (_, j) => ({
+    id: j,
+    name: `User ${j}`,
+  })),
+}));
+
 export default function MyPage() {
-  return <>MyPage</>;
+  return (
+    <>
+      <article>
+        <section className="flex flex-col gap-2">
+          <p className="text-xl text-center">ユーザー名</p>
+          <form className="flex gap-2 justify-center items-center">
+            <div className="flex gap-2 w-auto justify-center items-center">
+              <label htmlFor="name">名前</label>
+              <input
+                type="text"
+                placeholder="name"
+                className="input input-bordered w-2/3 max-w-xs"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              変更
+            </button>
+          </form>
+        </section>
+      </article>
+      <article className="grid grid-cols-1 gap-3 m-4">
+        <div className="collapse bg-base-200">
+          <input type="checkbox" name="my-accordion-1" />
+          <div className="collapse-title text-xl font-medium">作ったTodo</div>
+          <div className="collapse-content">
+            <ul className="grid grid-cols-1 gap-3 md:grid-cols-2">
+              {Todos.map((todo) => (
+                <li key={todo.id}>
+                  <TodoCard todo={todo} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="collapse bg-base-200">
+          <input type="checkbox" name="my-accordion-1" />
+          <div className="collapse-title text-xl font-medium">やったTodo</div>
+          <div className="collapse-content">
+            <ul className="grid grid-cols-1 gap-3 md:grid-cols-2">
+              {Todos.map((todo) => (
+                <li key={todo.id}>
+                  <TodoCard todo={todo} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </article>
+    </>
+  );
 }
